ad85eb7854522ef6ff2ca8ecd3a32e6c8d0857b2
taochanglian <taochanglian@163.com>
2017-01-11 11:47:48 +0800

    no message

---
+++ src/main/scala/com/test/spark/streamwordcount/LogLevels.scala
@@ -0,0 +1,18 @@
+package com.test.spark.streamwordcount
+
+import org.apache.log4j.{Level, Logger}
+import org.apache.spark.Logging
+
+/**
+  * Created by tao on 17/1/10.
+  */
+object LogLevels extends Logging{
+
+  def setStreamLogLevels(): Unit ={
+    val log4jInitialized = Logger.getRootLogger.getAllAppenders.hasMoreElements
+    if(log4jInitialized) {
+      logInfo("Setting log level to [WARn] from streaming example. To override add a customer log4j.properties to the classpath.")
+      Logger.getRootLogger.setLevel(Level.WARN)
+    }
+  }
+}
---
+++ src/main/scala/com/test/spark/streamwordcount/StreamWordCount.scala
@@ -0,0 +1,56 @@
+package com.test.spark.streamwordcount
+
+import org.apache.spark.streaming.{Seconds, StreamingContext}
+import org.apache.spark.{SparkContext, SparkConf}
+
+/**
+  * Created by tao on 17/1/10.
+  */
+object StreamWordCount {
+  def main(args: Array[String]) {
+    val conf = new SparkConf().setAppName("StreamWordCount")
+    val sc = new SparkContext(conf)
+    val ssc = new StreamingContext(sc,Seconds(5))
+
+    //接受数据
+    val ds = ssc.socketTextStream("127.0.0.1",8888)
+    //DStream是一个特殊的RDD
+    val result = ds.flatMap(_.split(" ")).map((_,1)).reduceByKey(_+_)
+    //数据存起来活着打印
+//    result.mapPartitions(it=> {
+//      val connection = null
+//      it.map(_)
+//    })
+      result.print()
+
+    //启动ds,等待结束
+    ssc.start()
+    ssc.awaitTermination()
+
+//接下来通过nc 命令开始写数据,就行了  nc -lk 8888
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
