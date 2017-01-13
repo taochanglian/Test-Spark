2516f753c4306ab4bb9d264e7e7950c279d48311
taochanglian <taochanglian@163.com>
2017-01-13 13:34:50 +0800

    no message

---
+++ src/main/scala/com/test/spark/kafka/KafkaDirectorWordCount.scala
@@ -0,0 +1,8 @@
+package com.test.spark.kafka
+
+/**
+  * Created by tao on 17/1/12.
+  */
+object KafkaDirectorWordCount {
+
+}
---
+++ src/main/scala/org/apache/spark/streaming/kafka/KafkaManager.scala
@@ -0,0 +1,8 @@
+package org.apache.spark.streaming.kafka
+
+/**
+  * Created by tao on 17/1/13.
+  */
+class KafkaManager {
+
+}
